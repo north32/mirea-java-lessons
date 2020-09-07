9f8ebb24c011f381e4a7e28824b5f6b53b9bc09e
north32 <sevaorlando@gmail.com>
2020-09-07 10:43:46 +0300

    updated last exercise

--- src/ru/north32/mireajava/exercise5/Main.java
+++ src/ru/north32/mireajava/exercise5/Main.java
@@ -3,7 +3,21 @@ package ru.north32.mireajava.exercise5;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(factorial(0) == 1);
+        System.out.println(factorial(1) == 1);
+        System.out.println(factorial(5) == 120);
+        System.out.println(factorial(-5) == -1);
+    }
 
+    public static int factorial(int n) {
+        if (n < 0) {
+            return -1;
+        }
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+          fact *= i;
+        }
+        return fact;
     }
 
 }
